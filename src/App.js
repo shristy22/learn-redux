30eaa88a48ad728ee1f542e313519a33f4e5cf02
shristy22 <shristy.sinha564@gmail.com>
2021-01-28 21:12:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment,decrement} from './actions';
 
 function App() {
+  const counter = useSelector(state=>state.counter);
+  const logged = useSelector(state=>state.logged);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <p>Hello</p>
+     <h2>counter is this:  {counter}</h2>
+     <button onClick={()=> dispatch(increment(5))}> + </button>
+     <button onClick={()=> dispatch(decrement())}>-</button>
+      {!logged ? <h1>logged in</h1> : ''}
     </div>
   );
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import counterReducer from './counter';
+import loggedReducer from './isLogged';
+import {combineReducers} from 'redux';
+
+const allReducer = combineReducers({
+    counter : counterReducer,
+    logged : loggedReducer
+});
+export default allReducer;
